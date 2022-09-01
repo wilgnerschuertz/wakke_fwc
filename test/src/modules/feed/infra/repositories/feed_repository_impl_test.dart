14fd55291e1969b7d7c798512a35de845515618b
wilgnerschuertz <schuertzww@icloud.com>
2022-09-01 09:26:16 -0400

    test: Repository - FeedRepositoryImpl / Should return of list empty

--- test/src/modules/feed/infra/repositories/feed_repository_impl_test.dart
+++ test/src/modules/feed/infra/repositories/feed_repository_impl_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:wakke_fwc/src/modules/feed/domain/errors/errors.dart';
 import 'package:wakke_fwc/src/modules/feed/infra/datasource/feed_datasource.dart';
 import 'package:wakke_fwc/src/modules/feed/infra/models/feed_model.dart';
 import 'package:wakke_fwc/src/modules/feed/infra/repositories/feed_repository_impl.dart';
@@ -34,5 +35,16 @@ void main() {
       // asset
       expect(result.fold(id, id), isA<List<FeedModel>>());
     });
+  });
+
+  test('Should return of list empty', () async {
+    // arrange
+    when(_datasource.getFeeds()).thenAnswer(
+      (_) async => <FeedModel>[],
+    );
+    // act
+    var result = await sut.getFeed();
+    // asset
+    expect(result.fold(id, id), isA<EmptyListError>());
   });
 }
