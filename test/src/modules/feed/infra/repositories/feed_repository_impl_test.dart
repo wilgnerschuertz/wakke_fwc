c32ccae1b22574d35aa8e857c2a64592fa678793
wilgnerschuertz <schuertzww@icloud.com>
2022-09-01 09:25:20 -0400

    test: Repository - FeedRepositoryImpl / Should return of list feed

---
+++ test/src/modules/feed/infra/repositories/feed_repository_impl_test.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wakke_fwc/src/modules/feed/infra/datasource/feed_datasource.dart';
+import 'package:wakke_fwc/src/modules/feed/infra/models/feed_model.dart';
+import 'package:wakke_fwc/src/modules/feed/infra/repositories/feed_repository_impl.dart';
+
+import 'feed_repository_impl_test.mocks.dart';
+
+@GenerateMocks([FeedDatasource, FeedModel])
+void main() {
+  late MockFeedDatasource _datasource;
+  //System under testing
+  late FeedRepositoryImpl sut;
+
+  List<FeedModel> feedsMock = [
+    MockFeedModel(),
+  ];
+
+  setUpAll(() {
+    _datasource = MockFeedDatasource();
+    sut = FeedRepositoryImpl(_datasource);
+  });
+
+  group('Repository - FeedRepositoryImpl / ', () {
+    test('Should return of list feed', () async {
+      // arrange
+      when(_datasource.getFeeds()).thenAnswer(
+        (_) async => feedsMock,
+      );
+      // act
+      var result = await sut.getFeed();
+      // asset
+      expect(result.fold(id, id), isA<List<FeedModel>>());
+    });
+  });
+}
